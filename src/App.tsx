e1f36a2ea15c8e04f3e578d68d7111a2fc398b49
Ankush9120 <ankushgupta9120@gmail.com>
2024-07-10 19:54:13 +0530

    updated dropdown example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Dropdown } from './stories/Dropdown'
+  import React from 'react'
+  import { Dropdown } from './stories/Dropdown'
 
-const data = [
-  "Mic",
-  "Om",
-  "Keshav",
-  "Shivam",
-  "Kasyap",
-  "Henry"
-]
+  const data = [
+    "John",
+    "Emily ( She is very healthy and athletic, she has red hair )",
+    "Michael",
+    "Sophia",
+    "David",
+    "Jessica",
+    "William",
+    "Olivia",
+    "Benjamin",
+    "Emma"
+  ]
 
-const App: React.FC = () => {
-  return (
-    <Dropdown items={data} activeItemIndex={1} type={"SingleNoIcon"} />
-  )
-}
+  const App: React.FC = () => {
+    return (
+      <Dropdown items={data} labelVisibility='Visible' label='Select User' leftIconVisibility='Visible' labelIconVisibility='Visible' rightIconVisibility='Visible' text='select something' activeItemIndex={2} type={"Multi"} clearable='Yes' helperText="please select any user" />
+    )
+  }
 
-export default App
+  export default App
